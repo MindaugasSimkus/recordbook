3635b6ea57d9a26e646049d2b80a3265bcc4e0d1
MindaugasSimkus <shimkus.mindaugas@gmail.com>
2017-07-03 21:25:25 +0300

    start of the homework

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+//sesijos kintamieji
+
+session_start();
+
+
+
+if (isset($_POST['record'])) {
+	
+	$_SESSION['session_template'] = $_POST['record'];
+} else if (isset($_SESSION['session_template'])) {
+	$template_name = $_SESSION['session_template'];
+} else {
+	$template_name = $templates[0];
+}
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title> </title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+</head>
+<body>
+	<form action="" method="POST" class="form-inline">
+		<div class="form-group">
+		<label for="message">Message</label>
+			<textarea class="form-control" name="message" id="message" rows="3"></textarea>
+			
+		</div>
+	<button class="btn" type="submit">Add</button>
+</form>
+</body>
+</html>
